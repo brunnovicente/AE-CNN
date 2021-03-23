8a2ea37592be95c9af682053e36680bf83742550
Bruno Vicente <brunovicente.lima@ifma.edu.br>
2021-03-23 07:29:48 -0300

    Update ae-cnn.py

--- ae-cnn.py
+++ ae-cnn.py
@@ -1,6 +1,10 @@
 import warnings
 warnings.filterwarnings("ignore")
 
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,7 +16,8 @@ from keras.optimizers import SGD #Classe que permite trabalhar com o Otmizador a
 from keras.utils import to_categorical #Função que permite transformar as saídas de 0, 1, 2 para [0,0,0], [0,1,0], [0,0,1]
 from sklearn.metrics import accuracy_score #Funções para calcular a acurácia do modelo
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+#(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
 X_train = X_train.reshape((60000,28,28,1))/255
 X_test = X_test.reshape((10000,28,28))/255
